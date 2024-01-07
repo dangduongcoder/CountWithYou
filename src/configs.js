74b377a49bdd4c5d674dd3bc74b5ec5cc34b237e
dangduongcoder <truongdangduong666@gmail.com>
2024-01-07 13:32:44 +0700

    release: almost done
    done:
    - layout
    - responsive
    - js logic

---
+++ src/configs.js
@@ -0,0 +1,7 @@
+const CONFIG = {
+    loverName : 'Hang',
+    startDate : '2023-12-24',
+    headerTitle : 'THE BEST LOVE FOR YOU',
+}
+
+export default CONFIG
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,20 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily : {
+        'serif' : 'DM Serif Display',
+        'script' : 'Dancing Script',
+        'kodchasan' : 'Kodchasan',
+        'lateef' : 'Lateef',
+        'crushed' : 'Crushed',
+      },
+      colors : {
+        'hd-red' : '#ED6C5D',
+        'hd-gray' : '#00000082',
+        'hd-dark' : '#000000',
+      }
+    },
   },
   plugins: [],
 }
